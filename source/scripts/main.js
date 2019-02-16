ffa39831caf0725410dc195cbc2299620c02b568
ayshih2 <ayshih2@illinois.edu>
2019-02-16 11:49:45 -0600

    slideshow is working

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -78,11 +78,46 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('tracklistBttn').addEventListener('click', function() { modal.style.display = "block";}, false);
 	document.getElementsByClassName("close")[0].addEventListener('click', function(){ modal.style.display = "none"; }, false);
 
+	// for carosusel
+
+	document.getElementsByClassName('prev')[0].addEventListener('click', function(){ plusSlides(-1); }, false);
+	document.getElementsByClassName('next')[0].addEventListener('click', function(){ plusSlides(1); }, false);
+
 	window.addEventListener('click', function(event) {
 		if (event.target == modal) {
     	modal.style.display = "none";
   	}
 	});
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	// Next/previous controls
+	function plusSlides(n) {
+  	showSlides(slideIndex += n);
+	}
+
+	// Thumbnail image controls
+	function currentSlide(n) {
+  	showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+  	var i;
+  	var slides = document.getElementsByClassName("slides");
+  	///var dots = document.getElementsByClassName("dot");
+  	if (n > slides.length) {slideIndex = 1} 
+  	if (n < 1) {slideIndex = slides.length}
+  	for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  	}
+  	//for (i = 0; i < dots.length; i++) {
+    //  dots[i].className = dots[i].className.replace(" active", "");
+  	//}
+  	slides[slideIndex-1].style.display = "block"; 
+  	//dots[slideIndex-1].className += " active";
+	}
+
 }, false);
 
 
